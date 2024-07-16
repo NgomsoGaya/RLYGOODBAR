6b5551f7203f8469561dd27556cd499872b2aa4c
NgomsoGaya <ngomso.gaya09@gmail.com>
2024-07-16 15:10:22 +0200

    added gtag-js

--- script.js
+++ script.js
@@ -9,3 +9,38 @@ function showCategory(category) {
 function buyTicket() {
     alert("Ticket purchase functionality is not implemented yet.");
 }
+
+// Example for product impression (Menu item viewed)
+gtag('event', 'view_item', {
+    "items": [{
+      "id": "meal123",
+      "name": "Grilled Chicken",
+      "category": "Meals",
+      "price": 12.99
+    }]
+  });
+  
+  // Example for product click (Menu item clicked)
+  gtag('event', 'select_content', {
+    "items": [{
+      "id": "meal123",
+      "name": "Grilled Chicken",
+      "category": "Meals",
+      "price": 12.99
+    }]
+  });
+  
+  // Example for purchase (Ticket bought)
+  gtag('event', 'purchase', {
+    "transaction_id": "T12345",
+    "value": 25.99,
+    "currency": "USD",
+    "items": [{
+      "id": "ticket123",
+      "name": "Jane Doe Concert Ticket",
+      "category": "Events",
+      "price": 25.99,
+      "quantity": 1
+    }]
+  });
+  
